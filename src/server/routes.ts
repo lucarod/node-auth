29f5edad7e7e61111e616473aca04f335c7d90d7
Luca Rodrigues <luca12rodrigues@gmail.com>
2024-02-16 13:07:10 -0300

    feat: add dotenv; connect signin route to signin controller

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import { SignInController } from '@app/controllers/SignInController';
 import { SignUpController } from '@app/controllers/SignUpController';
+import { SignInUseCase } from '@app/useCases/SignInUseCase';
 import { SignUpUseCase } from '@app/useCases/SignUpUseCase';
 
 const router = Router();
 
-router.get('/signup', async (request, response) => {
+router.post('/signup', async (request, response) => {
   const SALT = 10;
   const signUpUseCase = new SignUpUseCase(SALT);
   const signUpController = new SignUpController(signUpUseCase);
@@ -17,10 +19,15 @@ router.get('/signup', async (request, response) => {
   response.status(statusCode).json(body);
 });
 
-router.get('/signin', (request, response) => {
-  response.status(200).json({
-    message: 'signin',
+router.post('/signin', async (request, response) => {
+  const signInUseCase = new SignInUseCase();
+  const signInController = new SignInController(signInUseCase);
+
+  const { statusCode, body } = await signInController.handle({
+    body: request.body,
   });
+
+  response.status(statusCode).json(body);
 });
 
 export { router };
